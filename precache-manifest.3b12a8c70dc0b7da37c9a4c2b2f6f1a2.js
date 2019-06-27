24ccd9483f564fdb8a43f7d0ea656904bee837b6
3dd13 <tatonlto@gmail.com>
2019-06-27 20:30:11 +0800

    deploy to gh-pages

---
+++ precache-manifest.3b12a8c70dc0b7da37c9a4c2b2f6f1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bfec33ede96494e3b8d",
+    "url": "/hk_election_result_history/js/about.994d4ad0.js"
+  },
+  {
+    "revision": "e5e2da953d5d61ac28ba",
+    "url": "/hk_election_result_history/css/app.ddfebc98.css"
+  },
+  {
+    "revision": "e5e2da953d5d61ac28ba",
+    "url": "/hk_election_result_history/js/app.110056be.js"
+  },
+  {
+    "revision": "d021b79bf0d946b241e9",
+    "url": "/hk_election_result_history/js/chunk-vendors.803af90d.js"
+  },
+  {
+    "revision": "2d30109468d07c2cf49ad7894508109f",
+    "url": "/hk_election_result_history/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hk_election_result_history/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hk_election_result_history/precache-manifest.3b12a8c70dc0b7da37c9a4c2b2f6f1a2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "hk-election-result-history"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
