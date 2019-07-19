cf7135bd63361c95a05fe3ec7bd496f6091b4b15
3dd13 <tatonlto@gmail.com>
2019-07-19 14:41:42 +0800

    refactor(sidebar): extract components

--- src/app.vue
+++ src/app.vue
@@ -24,43 +24,7 @@
 
     <div class="flex flex-col md:flex-row bg-teal-800">
       <div class="h-16 bottom-0 md:relative md:h-screen z-10 w-full md:w-48 bg-teal-800">
-        <div class="mt-4 sm:mt-20 md:mt-40 lg:mt-20 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
-          <ul class="list-reset flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
-            <li class="mr-3 flex-1">
-              <router-link
-                to="/election_types/legco"
-                class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-600"
-                active-class="text-white border-green-600"
-              >
-                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-300 block md:inline-block">
-                  立法會選舉
-                </span>
-              </router-link>
-            </li>
-            <li class="mr-3 flex-1">
-              <router-link
-                to="/election_types/distco"
-                class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-600"
-                active-class="text-white border-green-600"
-              >
-                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-300 block md:inline-block">
-                  區議會選舉
-                </span>
-              </router-link>
-            </li>
-            <li class="mr-3 flex-1">
-              <router-link
-                to="/election_types/rural"
-                class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-600"
-                active-class="text-white border-green-600"
-              >
-                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-300 block md:inline-block">
-                  鄉郊代表選舉 [TODO]
-                </span>
-              </router-link>
-            </li>
-          </ul>
-        </div>
+        <Sidebar></Sidebar>
       </div>
 
       <div class="main-content flex-1 bg-gray-100 md:mt-16 pb-24 md:pb-5">
@@ -69,6 +33,21 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { map } from 'lodash';
+
+import Sidebar from './components/sidebar.vue';
+
+@Component({
+  components: {
+    Sidebar,
+  },
+})
+export default class App extends Vue {
+}
+</script>
 
 <style>
 #app {
